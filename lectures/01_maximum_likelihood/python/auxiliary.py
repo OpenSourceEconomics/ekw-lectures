5afcadcf4640fb86287275d7518fc2d3578dbb5e
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2019-11-18 06:50:12 +0100

    cleanup lectures

---
+++ lectures/01_maximum_likelihood/python/auxiliary.py
@@ -0,0 +1,79 @@
+import pickle as pkl
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import respy as rp
+import numpy as np
+
+def plot_profile_likelihood(params_base, options_base, df):
+    
+    crit_func = rp.get_crit_func(params_base, options_base, df)
+
+    for index in params_base.index:
+
+        upper, lower = params_base.loc[index][["upper", "lower"]]
+        grid = np.linspace(lower, upper, 20)
+
+        fvals = list()
+        for value in grid:
+            params = params_base.copy()
+            params.loc[index, "value"] = value
+            fvals.append(crit_func(params))
+
+        fig, ax = plt.subplots()
+
+        ax.axvline(params_base.loc[index, "value"], color="#A9A9A9", linestyle="--", label="Baseline")
+        ax.plot(grid, np.array(fvals) - np.max(fvals))
+        ax.set_title(index)
+        plt.show()
+        
+def plot_score_function(norm_grid, norm_fds):
+    
+    fig, ax = plt.subplots()
+
+    ax.set_title("$\delta$")
+
+    ax.plot(norm_grid, norm_fds)
+    ax.plot(norm_grid, norm_grid)
+
+    plt.show()
+    
+
+def plot_bootstrap_distribution():
+    
+    index = ("delta", "delta")
+    for is_perturb in [True, False]:
+
+        fname = f"material/bootstrap.delta_perturb_{str(is_perturb).lower()}.pkl"
+        boot_params = pd.read_pickle(fname)
+
+        fig, ax = plt.subplots()
+
+        ax.hist(boot_params.loc[index, :], density=True)
+        ax.set_title(f"{index}, perturbed {is_perturb}")
+        ax.set_xlim(0.945, 0.955)
+
+        plt.show()
+        
+def plot_score_distribution():
+    
+    rslts = pkl.load(open(f"material/score.delta.pkl", "rb"))
+
+    fig, ax = plt.subplots()
+
+    ax.hist(rslts, density=True)
+    ax.set_title("$\delta$");
+    
+def plot_smoothing_parameter(rslts, params_base, grid):
+    
+    fig, ax = plt.subplots()
+
+    index = ("delta", "delta")
+
+    ax.axvline(params_base.loc[index, "value"], color="#A9A9A9", linestyle="--")
+
+    for tau, fvals in rslts.items():
+         ax.plot(grid, fvals, label=f"{tau}")
+
+    ax.set_title("$\delta$")
+    ax.legend()
---
+++ scripts/run_notebooks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""This module executes all notebooks. It serves the main purpose to ensure that all can be
+executed and work proper independently."""
+import subprocess as sp
+import glob
+import os
+
+os.chdir(os.environ['PROJECT_ROOT'] + '/lectures')
+
+
+for dir_ in glob.glob("*"):
+    
+    os.chdir(dir_)
+    
+    for notebook in sorted(glob.glob('*.ipynb')):
+        cmd = ' jupyter nbconvert --execute {}  --ExecutePreprocessor.timeout=-1'.format(notebook)
+        sp.check_call(cmd, shell=True)
+
+    os.chdir("../")
+                      
+    
