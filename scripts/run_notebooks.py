329c2b3ab8ce725fc045b6906aa3b182e31a8be3
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2019-12-02 13:03:54 +0100

    debug notebook

--- scripts/run_notebooks.py
+++ scripts/run_notebooks.py
@@ -13,6 +13,7 @@ for dir_ in glob.glob("*"):
     os.chdir(dir_)
     
     for notebook in sorted(glob.glob('*.ipynb')):
+        print(notebook)
         cmd = ' jupyter nbconvert --execute {}  --ExecutePreprocessor.timeout=-1'.format(notebook)
         sp.check_call(cmd, shell=True)
 
