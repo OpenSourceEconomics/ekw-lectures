52cc472f0985746808518f3375a3ff5b1ba967a8
Annica Gehlen <ama.gehlen@gmail.com>
2020-04-23 21:19:26 +0200

    Adjusted remaining notebook to respy msm interface.

--- lectures/02_method_of_simulated_moments/python/auxiliary_plots.py
+++ lectures/02_method_of_simulated_moments/python/auxiliary_plots.py
@@ -1,4 +1,6 @@
-""" Auxiliary file containing functions for plots for the notebook on simulated method of moments estimation.
+""" 
+Auxiliary file containing functions for plots for the notebook on simulated 
+method of moments estimation.
 """
 import copy
 import numpy as np
@@ -8,19 +10,24 @@ import respy as rp
 
 
 def plot_criterion_params(params, param_names, criterion_msm, radius): 
-    """Plot criterion_msm for all values in params given radius (percentage deviation from value in 
-       params dataframe). 
+    """
+    Plot criterion_msm for all values in params given radius (percentage 
+    deviation from value in params dataframe). 
     """
     
     true_values = [params.loc[name, "value"] for name in param_names]
-    deviations = [abs(val)*radius for val in true_values]
-    lbounds = [params.loc[name, "value"] - dev for name, dev in zip(param_names, deviations)]
-    ubounds = [params.loc[name, "value"] + dev for name, dev in zip(param_names, deviations)]
+    deviations = [abs(val)*radius if abs(val)!=0 else radius for val in true_values]
+    
+    lbounds = [params.loc[name, "value"] - dev for name, dev in zip(param_names, 
+                                                                    deviations)]
+    ubounds = [params.loc[name, "value"] + dev for name, dev in zip(param_names, 
+                                                                    deviations)]
     detail = 20         
     
     for idx in range(len(param_names)):        
         parameters = params.copy()
-        # Define grid of parameter values and calculate the criterion function value for this grid.
+        # Define grid of parameter values and calculate the criterion function 
+        # value for this grid.
         x_grid = np.linspace(lbounds[idx], ubounds[idx], detail)
         fvals_grid = ([])
         for param in x_grid:
@@ -31,25 +38,30 @@ def plot_criterion_params(params, param_names, criterion_msm, radius):
         # Plot criterion function for the calculated values. 
         plt.xticks(np.linspace(lbounds[idx], ubounds[idx],num=5))
         plt.plot(x_grid, fvals_grid)
-        plt.axvline(true_values[idx], color="#A9A9A9", linestyle="--", label="True Value")
+        plt.axvline(
+                    true_values[idx], 
+                    color="#A9A9A9", 
+                    linestyle="--", 
+                    label="True Value"
+                    )
         plt.xlabel(param_names[idx])
         
         plt.show()
     
     
 def plot_moments_choices(moments_obs, moments_sim):
+    """Plot choice frequencies for observed and simulated moments."""
     
     moments = [moments_obs, moments_sim]
     titles = ["Data Moments", "Simulated Moments"]
     
     for idx in [0,1]:
-        df = pd.DataFrame(moments[idx]['Choice Frequencies']).transpose()
-        df = df.rename(columns={0: "fishing", 1: "hammock"})
+        df = moments[idx]["Choice Frequencies"]
 
         plt.subplot(1, 2, idx+1)
         plt.ylim((0,1))
         plt.title(titles[idx])
-        plt.plot(pd.DataFrame(moments[idx]['Choice Frequencies']).transpose())
+        plt.plot(df)
         plt.xlabel("Period")
         plt.ylabel("Choice Frequencies")
         if idx == 1:
@@ -60,20 +72,21 @@ def plot_moments_choices(moments_obs, moments_sim):
     
     
 def plot_moments_wage(moments_obs, moments_sim):
+    """Plot Wage Distribution for observed and simulated moments."""
     
-    df_sim = pd.DataFrame(moments_sim['Wage Distribution']).transpose()    
-    df_obs = pd.DataFrame(moments_obs['Wage Distribution']).transpose()    
+    df_sim = pd.DataFrame(moments_sim['Wage Distribution'])    
+    df_obs = pd.DataFrame(moments_obs['Wage Distribution'])    
     
     plt.subplot(1, 2, 1)
     plt.title('Mean')
-    plt.plot(df_obs[0])
-    plt.plot(df_sim[0])
+    plt.plot(df_obs['mean'])
+    plt.plot(df_sim['mean'])
     plt.xlabel('Period')
     
     plt.subplot(1, 2, 2)
     plt.title('Std')
-    plt.plot(df_obs[1], label = "observed")
-    plt.plot(df_sim[1], label = "simulated")
+    plt.plot(df_obs['std'], label = "observed")
+    plt.plot(df_sim['std'], label = "simulated")
     plt.legend(loc="best")
     plt.xlabel('Period')
     
@@ -81,30 +94,36 @@ def plot_moments_wage(moments_obs, moments_sim):
     plt.show()
     
     
-def plot_chatter(seeds, criterion_smm, kwargs):
-    
+def plot_chatter(seeds, kwargs):
+    """Plot criterion function value at different seeds."""
     args = copy.deepcopy(kwargs)
     criterion_values =[]
     
     for seed in seeds:
         args['options']['simulation_seed'] = seed
-        val = criterion_smm(args['params'], 
-                            args['options'], 
-                            args['weighting_matrix'],
-                            args['moments_obs'],
-                            args['choice_options'])
+        criterion_msm = rp.get_msm_func(
+            params=args["params"], 
+            options=args["options"], 
+            calc_moments=args["calc_moments"], 
+            replace_nans= args["replace_nans"], 
+            empirical_moments=args["moments_obs"], 
+            weighting_matrix=args["weighting_matrix"]
+        )
+
+        val = criterion_msm(args["params"])
 
         criterion_values.append(val)
 
     plt.plot(seeds, criterion_values, color='C1')
-    plt.ylim(-1,40)
+    plt.ylim(-1,200)
     plt.title('Criterion function for different simulation seeds')
     plt.ylabel('Criterion function')
-    plt.xlabel('Seed') 
+    plt.xlabel('Seed')  
     
 
-def plot_chatter_numagents_sim(seeds, num_agents, criterion_smm, kwargs):
-    
+def plot_chatter_numagents_sim(seeds, num_agents, kwargs):
+    """Plot criterion function value at different seeds and numbers of agents
+    for the simulated sample."""
     args = copy.deepcopy(kwargs)
     results = pd.DataFrame(columns=num_agents)
 
@@ -113,27 +132,39 @@ def plot_chatter_numagents_sim(seeds, num_agents, criterion_smm, kwargs):
         criterion_values =[]
         for seed in seeds:
             args['options']['simulation_seed'] = seed
+            criterion_msm = rp.get_msm_func(
+                params=args["params"], 
+                options=args["options"], 
+                calc_moments=args["calc_moments"], 
+                replace_nans= args["replace_nans"], 
+                empirical_moments=args["moments_obs"], 
+                weighting_matrix=args["weighting_matrix"]
+            )
 
-            val = criterion_smm(args['params'], 
-                                args['options'], 
-                                args['weighting_matrix'],
-                                args['moments_obs'],
-                                args['choice_options'])
+            val = criterion_msm(args['params'])
 
             criterion_values.append(val)
 
         results[num] = criterion_values
 
         plt.plot(seeds, results[num], label=num)
-        plt.ylim(-1,40)
+        plt.ylim(-1,200)
         plt.title('Increasing only the number of simulated agents')
         plt.ylabel('Criterion function')
         plt.xlabel('Seed')
         plt.legend(loc='best')
         
         
-def plot_chatter_numagents_both(seeds, num_agents, get_moments, criterion_smm, kwargs):
-    
+def plot_chatter_numagents_both(
+    seeds, 
+    num_agents, 
+    calc_moments, 
+    replace_nans, 
+    kwargs
+):
+    """Plot criterion function for different seeds and numbers of agents in 
+    simulated and observed sample.
+    """
     args = copy.deepcopy(kwargs)
     # Initialize df to hold results.
     results = pd.DataFrame(columns=num_agents)
@@ -144,27 +175,32 @@ def plot_chatter_numagents_both(seeds, num_agents, get_moments, criterion_smm, k
         options_true["simulation_agents"] = num
         simulate = rp.get_simulate_func(args['params'], options_true)
         data_true = simulate(args['params'])
-        moments_true = get_moments(data_true, args['choice_options'])
-
+        moments_true = {"Choice Frequencies": replace_nans(
+                            calc_moments["Choice Frequencies"](data_true)),
+                        "Wage Distribution": replace_nans(
+                            calc_moments["Wage Distribution"](data_true))}
     # Increase number of agents in simulated model.        
         options_chatter = args['options'].copy()
         options_chatter["simulation_agents"] = num 
         criterion_values =[]
         for seed in seeds:
             options_chatter["simulation_seed"] = seed
-            val = criterion_smm(args['params'], 
-                                options_chatter, 
-                                args['weighting_matrix'], 
-                                moments_true, 
-                                args['choice_options'])
-
+            criterion_msm = rp.get_msm_func(
+                                 params=args['params'], 
+                                 options=options_chatter,
+                                 calc_moments=args["calc_moments"], 
+                                 replace_nans= args["replace_nans"], 
+                                 empirical_moments=moments_true,
+                                 weighting_matrix=args['weighting_matrix']
+                                                )
+            val = criterion_msm(args['params'])
             criterion_values.append(val)
 
         results[num] = criterion_values
 
     # Plot the results.
         plt.plot(seeds, results[num], label=num)
-        plt.ylim(-1,40)
+        plt.ylim(-1,200)
         plt.title('Increasing the number of Observed and Simulated agents')
         plt.ylabel('Criterion function')
         plt.xlabel('Seed')
